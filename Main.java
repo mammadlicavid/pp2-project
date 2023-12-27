a7a9db455a78ce2017fa6240ffd34b0265ab28da
HSadikhov <144788477+HSadikhov@users.noreply.github.com>
2023-12-27 21:03:42 +0400

    Update Main.java
    changes (continued)

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        User u = new User("first", "second", "122345");
 
-        System.out.println(User.checkIfUserExists("second")); // check if it added to db
-        System.out.println(User.checkIfUserExists("firsst")); // check if non db file exist in db
-
-        System.out.println(User.login("second", "122345")); // true credentials
-        System.out.println(User.login("first", "seccond")); // false credentialss
+        SwingUI.genereateUI();
 
     }
 }
